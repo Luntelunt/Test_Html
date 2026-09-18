f1a329446649cae180c914e78a03a77b4467313b
Luntelunt <line.mh@hotmail.com>
2026-09-18 11:35:54 +0200

    ting og sageer

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-
+ const demo = document.getElementById("demo");
+ 
 window.addEventListener("message", function (event){
     const data = event.data;
+
     console.log("Yum", data);
+
 
     if (data === "BS=NEAR")
     {
-        this.document.body.style.background = "#df6b6b11";
+        //this.document.body.style.background = "#df6b6b11";
+        demo.textContent = "NEAAAR"
 
     }
     else if (data === "BS=FAR")
@@ -18,4 +22,32 @@ window.addEventListener("message", function (event){
     }
 
 
+})
+
+
+
+// const listen = document.getElementById("inTest");
+
+// listen.addEventListener("input", function (event)
+// {
+//     console.log("Input:", event.target.value);
+//     demo.textContent = event.target.value;
+// });
+
+// listen.addEventListener("keydown", function (event)
+// {
+//     if (event.code === "Enter")
+//     {
+//         demo.textContent = "Enter was pressed!";
+//         console.log("Enter virker");
+//     }
+// })
+
+window.addEventListener("keydown", function (event)
+{
+    if (event.code === "Enter")
+    {
+        demo.textContent = "Enter was pressed!";
+        console.log("Enter virker");
+    }
 })
